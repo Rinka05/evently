dd21148711c35f9ca74390a328d741703fddf499
Rinka <102121042@nitt.edu>
2024-07-26 12:09:43 +0530

    test2

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -14,9 +14,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  // const orders = await getOrdersByUser({ userId, page: ordersPage})
-
-  // const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
@@ -33,20 +31,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
-        <Collection 
-          data={orderedEvents}
-          emptyTitle="No event tickets purchased yet"
-          emptyStateSubtext="No worries - plenty of exciting events to explore!"
-          collectionType="My_Tickets"
-          limit={3}
-          page={ordersPage}
-          urlParamName="ordersPage"
-          totalPages={orders?.totalPages}
-        />
-      </section> */}
-
-      {/* Events Organized */}
+      
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
@@ -58,18 +43,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
-        <Collection 
-          data={organizedEvents?.data}
-          emptyTitle="No events have been created yet"
-          emptyStateSubtext="Go create some now"
-          collectionType="Events_Organized"
-          limit={3}
-          page={eventsPage}
-          urlParamName="eventsPage"
-          totalPages={organizedEvents?.totalPages}
-        />
-      </section> */}
+     
     </>
   )
 }
